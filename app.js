c67f1fa12d74bfc480cfa0da82944d30ba6f0af9
BlueSkyClouds <q1471223507@gmail.com>
2020-10-13 14:31:57 +0800

    增加COOKIES失效提示，取消日常签到提示

--- app.js
+++ app.js
@@ -51,8 +51,10 @@ async function start() {
     if (fs.existsSync(path)) {
         content = fs.readFileSync(path, "utf8");
     }
-    await notify.sendNotify("爱奇艺签到-" + new Date().toLocaleDateString(), content);
-    console.log("爱奇艺签到-" + content)
+    if (content.includes("Cookie")) {
+        await notify.sendNotify("爱奇艺签到-" + new Date().toLocaleDateString(), content);
+        console.log("爱奇艺签到-" + content)
+    }
 
     //运行完成后，删除下载的文件
     console.log('运行完成后，删除下载的文件\n')
