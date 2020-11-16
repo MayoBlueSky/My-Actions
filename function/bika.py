1c130ee7ea2a361ab023e5d7d20e1c0c922708b0
BlueskyClouds <q1471223507@gmail.com>
2020-11-17 02:33:23 +0800

    增加哔咔漫画

---
+++ function/bika.py
@@ -0,0 +1,94 @@
+import hashlib
+import hmac
+import json
+import random
+import string
+import sys
+import time
+
+username=os.environ['BIKA_USER']
+password=os.environ['BIKA_PASS']
+serverJ = os.environ['push_key']
+
+msg = ''
+
+# noinspection SpellCheckingInspection
+pica_api_host = "picaapi.picacomic.com"
+pica_api_base_url = "https://%s/" % pica_api_host
+sign_in_path = "auth/sign-in"
+punch_in_path = "users/punch-in"
+POST = "POST"
+
+# noinspection SpellCheckingInspection
+api_key = "C69BAF41DA5ABD1FFEDC6D2FEA56B"
+api_secret = "~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn"
+# noinspection SpellCheckingInspection
+static_headers = {
+    "api-key": api_key,
+    "accept": "application/vnd.picacomic.com.v1+json",
+    "app-channel": "2",
+    "app-version": "2.2.1.2.3.3",
+    "app-uuid": "defaultUuid",
+    "app-platform": "android",
+    "app-build-version": "44",
+    "User-Agent": "okhttp/3.8.1",
+    "image-quality": "original",
+}
+
+
+def send_request(path: string, method: string, body: string = None, token: string = None) -> dict:
+    current_time = str(int(time.time()))
+    nonce = "".join(random.choices(string.ascii_lowercase + string.digits, k=32))
+    raw = path + current_time + nonce + method + api_key
+    raw = raw.lower()
+    h = hmac.new(api_secret.encode(), digestmod=hashlib.sha256)
+    h.update(raw.encode())
+    signature = h.hexdigest()
+    headers = static_headers.copy()
+    headers["time"] = current_time
+    headers["nonce"] = nonce
+    headers["signature"] = signature
+    if body is not None:
+        headers["Content-Type"] = "application/json; charset=UTF-8"
+    if token is not None:
+        headers["authorization"] = token
+    connection = client.HTTPSConnection(pica_api_host)
+    connection.request(method, '/' + path, body, headers)
+    response = connection.getresponse().read().decode("utf-8")
+    json_object = json.loads(response)
+    if json_object["code"] != 200:
+        raise RuntimeError(json_object["message"])
+    return json_object
+
+def sign_in(email: string, password: string) -> string:
+    body = {
+        "email": email,
+        "password": password
+    }
+    return send_request(sign_in_path, POST, json.dumps(body))["data"]["token"]
+
+
+def punch_in(token: string):
+    return send_request(punch_in_path, POST, token=token)
+
+
+if __name__ == '__main__':
+    current_token = sign_in(username, password)
+    punch_in_response = punch_in(current_token)
+    result = punch_in_response["data"]["res"]
+    if result["status"] == "ok":
+        print("打卡成功, 最后一次打卡: %s" % result["punchInLastDay"])
+    else:
+        print("Already punch-in")
+        msg = '重复签到'
+
+# Server酱
+if serverJ != "":
+    api = "https://sc.ftqq.com/"+ serverJ + ".send"
+    title = u"哔咔漫画自动打哔咔"
+    content = msg
+    data = {
+        "text":title,
+        "desp":content
+    }
+    req = requests.post(api,data = data)
