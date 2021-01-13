8e1525e9494109394cffa629c718eafa9a7064ee
BlueskyClouds <q1471223507@gmail.com>
2021-01-13 16:12:09 +0800

    fix

--- function/sendNotify.py
+++ function/sendNotify.py
@@ -132,12 +132,13 @@ class sendNotify:
                 'Content-Type': 'application/json;charset=utf-8'
             }
             if sendNotify.DD_BOT_SECRET != '':
-                timestamp = long(round(time.time() * 1000))
-                secret_enc = bytes(secret).encode('utf-8')
+                timestamp = str(round(time.time() * 1000))
+                secret = sendNotify.DD_BOT_SECRET
+                secret_enc = secret.encode('utf-8')
                 string_to_sign = '{}\n{}'.format(timestamp, secret)
-                string_to_sign_enc = bytes(string_to_sign).encode('utf-8')
+                string_to_sign_enc = string_to_sign.encode('utf-8')
                 hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
-                sign = urllib.quote_plus(base64.b64encode(hmac_code))
+                sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
                 url = 'https://oapi.dingtalk.com/robot/send?access_token='+sendNotify.DD_BOT_TOKEN+'&timestamp='+timestamp+'&sign='+sign
 
             response = requests.post(url=url, data=json.dumps(data), headers=headers).text
