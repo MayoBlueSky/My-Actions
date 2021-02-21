4b0d8e85f444808f810c639709f3b3b5decbb976
BlueskyClouds <q1471223507@gmail.com>
2021-02-21 14:21:52 +0800

    update wps add sleep

--- function/wps_invitation.py
+++ function/wps_invitation.py
@@ -48,6 +48,7 @@ mass_limit = 5
 # 这13个账号被邀请,且参与WPS会员群集结,如群集结失败, 请修改以下sid, 修改时注意保留双引号
 invite_sid = [
         "V02SC1mOHS0RiUBxeoA8NTliH2h2NGc00a803c35002693584d",
+        "V02S2UBSfNlvEprMOn70qP3jHPDqiZU00a7ef4a800341c7c3b",
         "V02StVuaNcoKrZ3BuvJQ1FcFS_xnG2k00af250d4002664c02f",
         "V02SWIvKWYijG6Rggo4m0xvDKj1m7ew00a8e26d3002508b828",
         "V02Sr3nJ9IicoHWfeyQLiXgvrRpje6E00a240b890023270f97",
@@ -139,6 +140,7 @@ def main():
         sio.write("---为{}邀请---↓\n\n".format(item['name']))
         if type(resp['data']['userid']) == int:
             wps_miniprogram_invite(invite_sid, resp['data']['userid'])
+            time.sleep(5)
         else:
             sio.write("邀请失败: 用户ID错误, 请检查用户sid\n\n")
 
