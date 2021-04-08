afbe3bb45114254cedba7183c3332535370fdb5f
BlueskyClouds <q1471223507@gmail.com>
2021-04-09 03:04:28 +0800

    update

--- function/bilibili/bilibili.py
+++ function/bilibili/bilibili.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import requests
 import sys;
 sys.path.append("My-Actions/function/bilibili/")
 from bilibiliapi import *
 from sendNotify import *
-import requests
 
 sendNotify = sendNotify()
 SEND_KEY = os.environ['SEND_KEY']
