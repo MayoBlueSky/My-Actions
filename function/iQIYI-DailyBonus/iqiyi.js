7fa7ea09a130d07301426bb37e7be6fb8c229148
BlueskyClouds <q1471223507@gmail.com>
2021-04-20 11:47:00 +0800

    fuck me

--- function/iQIYI-DailyBonus/iqiyi.js
+++ function/iQIYI-DailyBonus/iqiyi.js
@@ -8,7 +8,7 @@ const download = require('download')
 const $ = new Env('爱奇艺会员签到');
 const notify = $.isNode() ? require('../sendNotify') : '';
 // 公共变量
-const KEY = "process.env.iQIYI_COOKIE"
+const KEY = process.env.iQIYI_COOKIE
 const SEND_KEY = process.env.SEND_KEY
 const UTC8 = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;
 
